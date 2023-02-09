793f94cd8f8e9948a3eda199f929d06de545bea8
Hugo <>
2023-02-09 17:03:39 +0100

    add time change

--- src/app/schedules/modal/timetable-modal/timetable-modal.component.ts
+++ src/app/schedules/modal/timetable-modal/timetable-modal.component.ts
@@ -20,12 +20,19 @@ export class TimeTableModalComponent {
 
   city1: string = ""
   city2: string = ""
+
   directionName1: string = ""
   directionName2: string = ""
+
+  date: string = '';
+  time: string = '';
 
   public segment: string = "direction1"
 
   ngOnInit() {
+    const now = new Date().toLocaleString()
+    this.date = now.split(" ")[0].split("/").reverse().join("-")
+    this.time = now.split(" ")[1].slice(0, -3)
     this.loadTimeTable()
     this.api.getClusters(this.line!.id).subscribe(clusters => {
       const cluster1 = clusters[0]
@@ -37,17 +44,30 @@ export class TimeTableModalComponent {
     })
   }
 
+  changeDate(e: Event) {
+      this.date = String(e)
+      this.loadTimeTable()
+  }
+
+  changeTime(e: Event) {
+    this.time = String(e)
+    this.loadTimeTable()
+}
+
   getTripTime(trip: number): string {
     const tripTime = new Date(trip * 1000)
     return `${tripTime.getHours().toString().padStart(2, "0")}:${tripTime.getMinutes().toString().padStart(2, "0")}`
   }
 
   loadTimeTable() {
-    this.api.getTimeTable(this.line?.id!, Date.now()).subscribe(data => {
-      console.log(data[0])
+    this.api.getTimeTable(this.line?.id!, this.toTimestamp()).subscribe(data => {
       this.direction1 = data[0]
       this.direction2 = data[1]
     })
+  }
+
+  private toTimestamp(): number{
+    return Date.parse(`${this.date} ${this.time}`)
   }
 
   cancel() {
